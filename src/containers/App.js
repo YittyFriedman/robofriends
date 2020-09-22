4295f8e97b5f4a89a3af2a16928709d69a152359
Yitty <yitty416@gma>
2020-09-22 12:49:41 -0400

    adding robofriends app to git repo

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,16 +25,14 @@ this.setState({ searchfield: event.target.value})
 
 }
 render(){
-	const filteredRobots = this.state.robots.filter(robots=>{
-	return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())})
-		
-if(this.state.robots.length === 0){
-	return <h1>Loading</h1>
-}
-else{
-		return(
+	const {robots, searchfield} = this.state;
+	const filteredRobots = robots.filter(robot=>{
+	return robot.name.toLowerCase().includes(searchfield.toLowerCase())})
+		return !robots.length ?
+	  <h1>Loading</h1> :
+(
 			<div className= 'tc'>
-		<h1 className='f1'>RoboFriends</h1>
+				<h1 className='f1'>RoboFriends</h1>
 				
 				<SearchBox searchChange={this.onSearchChange} />
 				<Scroll>
@@ -42,7 +40,8 @@ else{
 				</Scroll>
 			</div>
 		);
-	}}
+	
+}
 }
 
 export default App;
